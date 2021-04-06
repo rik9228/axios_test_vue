fa5c1312ee228bd839d85194fac3d26e652e1ae2
RIKU <rank228130@gmail.com>
2021-04-06 21:02:36 +0900

    [feat] Update App and Form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,38 @@
 <template>
-
+  <div class="container">
+    <h1>入力値が2倍になって返される処理</h1>
+    <input
+      type="number"
+      class="input"
+      :value="inputNumValue"
+    />
+    <button class="button" @click="onSubmit">送信</button>
+    <div class="result">{{ result }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "Form",
+  props: {
+    inputNumValue: {
+      require: true,
+    },
+    result: {
+      require: true,
+    },
+  },
+  methods: {
+    onSubmit: function () {
+      this.$emit("onSubmit", this.inputNumValue);
+    },
+  },
+};
 </script>
 
-<style>
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.button {
+  margin-left: 16px;
+}
 </style>
